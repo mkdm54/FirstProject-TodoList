b8b8a06160b51e034c9d0b5ef189d52bedbccbd1
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-04 21:59:12 +0700

    Tambahkan @csrf untuk perlindungan CSRF pada formulir

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,7 @@
             <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
         </div>
         <form class="w-full max-w-sm mx-auto px-4 py-2">
+            @csrf
             <div class="flex items-center border-b-2 border-teal-500 py-2">
                 <input
                     class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
