6dc0deb86d497a90bccd0e65550c76570cf86e3f
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-04 22:33:50 +0700

    fix bug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
         <div class="px-4 py-2">
             <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
         </div>
-        <form class="w-full max-w-sm mx-auto px-4 py-2" action="{{ $route('task.store') }}" method="POST">
+        <form class="w-full max-w-sm mx-auto px-4 py-2" action="{{ route('task.store') }}" method="POST">
             @csrf
             <div class="flex items-center border-b-2 border-teal-500 py-2">
                 <input
