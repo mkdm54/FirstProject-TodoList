6fff908ff27c839ffba731a786df06d2dc00dbdf
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-04 22:32:22 +0700

    Perbaiki action form untuk pembuatan task

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,12 +14,12 @@
         <div class="px-4 py-2">
             <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
         </div>
-        <form class="w-full max-w-sm mx-auto px-4 py-2">
+        <form class="w-full max-w-sm mx-auto px-4 py-2" action="{{ $route('task.store') }}" method="POST">
             @csrf
             <div class="flex items-center border-b-2 border-teal-500 py-2">
                 <input
                     class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    type="text" placeholder="Add a task">
+                    type="text" name="todo_tasks" placeholder="Add a task">
                 <button
                     class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
                     type="button">
