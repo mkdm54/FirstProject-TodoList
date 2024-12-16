5cc703282f3c9e8e4e6be2a68c1cb115b2cdf5eb
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-16 12:29:35 +0700

    Menambahkan penanganan pesan sukses dan error dalam JavaScript

--- resources/js/index/index.js
+++ resources/js/index/index.js
@@ -1,10 +1,54 @@
+const { error } = require("laravel-mix/src/Log");
+const { resolveConfig } = require("vite");
+
+const errorMessage = document.getElementById('error-alert-message');
+const successMessage = document.getElementById('success-alert-message');
+const successMessageContainer = document.getElementById('successMessageContainer');
+const errorMessageContainer = document.getElementById('errorMessageContainer');
+
+//Fungsi Tambah Tugas
+document.addEventListener('DOMContentLoaded', function () {
+    const formInputData = document.getElementById('form-input-data');
+
+    formInputData.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const inputData = document.getElementById('input-data').value.trim();
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        fetch('tasks/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+            body: JSON.stringify({ 'inputData': inputData })
+        }
+        ).then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to sumbit data ${response.statusText}`);
+            }
+            return response.json();
+        }).then(data => {
+            if (data.success) {
+                inputData.value = '';
+                window.location.href = '/'
+                successMessageContainer.classList.remove('hidden');
+                successMessage.innerHTML = data.message;
+            }
+            else {
+                errorMessageContainer.classList.remove('hidden');
+                errorMessage.innerHTML = data.message;
+            }
+        }).catch(error => {
+            console.log(`Error : ${error}`);
+        });
+    });
+});
+
+// Fungsi Hapus tugas
 window.deleteTask = function (taskId) {
     if (confirm("Apakah Anda yakin ingin menghapus tugas ini?")) {
         const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-        const errorMessage = document.getElementById('error-alert-message');
-        const successMessage = document.getElementById('success-alert-message');
-        const successMessageContainer = document.getElementById('successMessageContainer');
-        const errorMessageContainer = document.getElementById('errorMessageContainer');
         fetch(`/tasks/${taskId}`, {
             method: 'DELETE',
             headers: {
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,12 +52,12 @@
         <div class="px-4 py-2">
             <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
         </div>
-        <form class="w-full max-w-sm mx-auto px-4 py-2" action="{{ route('tasks.store') }}" method="POST">
+        <form class="w-full max-w-sm mx-auto px-4 py-2" action="{{ route('tasks.store') }}" id="form-input-data" method="POST">
             @csrf
             <div class="flex items-center border-b-2 border-teal-500 py-2">
                 <input
                     class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    type="text" name="todo_tasks" placeholder="Add a task">
+                    type="text" name="todo_tasks" placeholder="Add a task" id="input-data">
                 <button
                     class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
                     type="submit">
